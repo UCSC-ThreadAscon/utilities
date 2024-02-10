2d7d31264fb8566df423e015d2f70cbc20ea5e18
Simeon Tran <siatran@ucsc.edu>
2024-02-09 17:46:27 -0800

    Quick script

--- payload_size.py
+++ payload_size.py
@@ -8,10 +8,19 @@ import csv
   https://stackoverflow.com/a/38523398
 """
 def payloadSizeList(filename):
-  with open(filename) as file:
-    reader = csv.DictReader(csv)
-    for row in reader:
-      print(row['Length'])
-  return
+  lengths = []
 
-payloadSizeList('packets_decrypted.csv')
+  with open(filename) as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+      lengths.append(row['Length'])
+
+  return lengths
+
+decrypted = payloadSizeList('packets_decrypted.csv')
+encrypted = payloadSizeList('packets_encrypted.csv')
+
+for i in range(0, min(len(decrypted), len(encrypted))):
+  csvRow = i + 2
+  difference = abs(int(decrypted[i]) - int(encrypted[i]))
+  print(f"Row {csvRow} in CSV: Difference between length {decrypted[i]} and {encrypted[i]} is {difference}")
