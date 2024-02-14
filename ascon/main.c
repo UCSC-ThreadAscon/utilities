5c5ec8634d4ea9d842af8704b81415960d41c311
Simeon Tran <siatran@ucsc.edu>
2024-02-14 11:59:26 -0800

    Text to be encrypted

--- ascon/main.c
+++ ascon/main.c
@@ -1,7 +1,18 @@
 #include "core.h"
 #include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
 
 int main() {
-  printf("Hello world!");
+  const unsigned char tag = 186;
+  const unsigned char nonce = 187;
+  const unsigned char key = 130;
+
+  void* plaintext = calloc(40, sizeof(char));
+  char* string = "The brown fox jumped over the lazy dog.";
+  memcpy(plaintext, string, sizeof(char) * 40);
+  printf("Text to be encrypted: %s\n", (char *) plaintext);
+
+  free(plaintext);
   return 0;
 }
