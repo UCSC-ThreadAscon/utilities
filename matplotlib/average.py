68a1f9bd2369ddcdc4d3da5879af5719c6a2e09b
Simeon Tran <siatran@ucsc.edu>
2024-04-18 15:05:29 -0700

    Get the average microamps

--- matplotlib/average.py
+++ matplotlib/average.py
@@ -3,3 +3,16 @@
   will return the average energy consumption in both
   mA and mAh.
 """
+import csv
+
+def getMicroAmpsList(filename):
+  microAmpsList = []
+
+  with open(filename) as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+      microAmpsList.append(row['Current(uA)'])
+
+  return microAmpsList
+
+print(getMicroAmpsList('aes-0dbm-frontdoor-sample.csv'))
