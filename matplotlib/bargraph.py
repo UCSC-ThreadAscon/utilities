77871465e1e08da4f8221b9f76bba1c1f7351f21
Simeon Tran <siatran@ucsc.edu>
2024-04-18 14:30:43 -0700

    Saving what I wrote so far.

---
+++ matplotlib/bargraph.py
@@ -0,0 +1,30 @@
+import matplotlib.pyplot as plt
+import numpy as np
+
+txpower = ("20 dBm", "0dBm", "-9 dBm")
+penguin_means = {
+    'AES-CCM': (18.35, 18.43, 14.98),
+    'ASCON-128a': (38.79, 48.83, 47.50),
+    'ASCON-128': (189.95, 195.82, 217.19),
+}
+
+x = np.arange(len(txpower))  # the label locations
+width = 0.25  # the width of the bars
+multiplier = 0
+
+fig, ax = plt.subplots(layout='constrained')
+
+for attribute, measurement in penguin_means.items():
+    offset = width * multiplier
+    rects = ax.bar(x + offset, measurement, width, label=attribute)
+    ax.bar_label(rects, padding=3)
+    multiplier += 1
+
+# Add some text for labels, title and custom x-axis tick labels, etc.
+ax.set_ylabel('Average Energy Consumption (mA) ')
+ax.set_title('Front Door ESP32-H2')
+ax.set_xticks(x + width, txpower)
+ax.legend(loc='upper left', ncols=3)
+ax.set_ylim(0, 250)
+
+plt.show()
