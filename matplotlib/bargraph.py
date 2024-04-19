0246e20462a5a0a27c3b0fd0da38302c55805f9e
Simeon Tran <siatran@ucsc.edu>
2024-04-18 21:19:10 -0700

    Bar graph works

--- matplotlib/bargraph.py
+++ matplotlib/bargraph.py
@@ -26,14 +26,17 @@ fig, ax = plt.subplots(layout='constrained')
 for attribute, measurement in mean_energy_usage.items():
     offset = width * multiplier
     rects = ax.bar(x + offset, measurement, width, label=attribute)
-    ax.bar_label(rects, padding=3)
+    ax.bar_label(rects, padding=4)
     multiplier += 1
 
 # Add some text for labels, title and custom x-axis tick labels, etc.
 ax.set_ylabel('Average Energy Consumption (mA)')
 ax.set_title('Front Door ESP32-H2')
 ax.set_xticks(x + width, txpower)
-ax.legend(loc='upper left', ncols=3)
-ax.set_ylim(0, 4)
+
+y_lim = 3.5
+ax.set_yticks(np.arange(0, y_lim, 0.3))
+ax.legend(loc='upper right', ncols=3)
+ax.set_ylim(0, y_lim)
 
 plt.show()
