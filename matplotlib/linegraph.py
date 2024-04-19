7f99ddbfc40940c615f682304be800fa738dce84
Simeon Tran <siatran@ucsc.edu>
2024-04-19 11:24:22 -0700

    Can create plots from list.

--- matplotlib/linegraph.py
+++ matplotlib/linegraph.py
@@ -6,8 +6,16 @@ from common import *
 # evenly sampled time at 200ms intervals
 t = np.arange(0., 5., 1)
 
+ax = plt.axes()
+
+y_lim = 100
+ax.set_yticks(np.arange(0, y_lim + 10, 10))
+ax.legend(loc='upper right', ncols=3)
+ax.set_ylim(0, y_lim)
+
 # red dashes, blue squares and green triangles
-plt.plot(t, t, 'o--r', color=cipherToColor['AES-CCM'])
-plt.plot(t, t**2, 'o:b', color=cipherToColor['ASCON-128a'])
-plt.plot(t, t**3, 'o-.g', color=cipherToColor['ASCON-128'])
+plt.plot([5, 10, 15], 'o--r', color=cipherToColor['AES-CCM'])
+plt.plot([10, 20, 30], 'o:b', color=cipherToColor['ASCON-128a'])
+plt.plot([30, 40, 60], 'o-.g', color=cipherToColor['ASCON-128'])
+
 plt.show()
