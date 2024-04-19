686bd3ed692038e3444eee56c21efc724446a77e
Simeon Tran <siatran@ucsc.edu>
2024-04-19 11:47:00 -0700

    Line graph works git push

--- matplotlib/linegraph.py
+++ matplotlib/linegraph.py
@@ -3,19 +3,29 @@ import numpy as np
 
 from common import *
 
-# evenly sampled time at 200ms intervals
-t = np.arange(0., 5., 1)
+tx_powers = [-9, 0, 20]
+y_lim = 15
+y_min = -15
+y_interval = 5
 
-ax = plt.axes()
+fig, ax = plt.subplots()
 
-y_lim = 100
-ax.set_yticks(np.arange(0, y_lim + 10, 10))
+ascon128a_lines = plt.plot(tx_powers, [-8, -1, 13], 'o:b',
+                           color=cipherToColor['ASCON-128a'],
+                           label='ASCON-128a')
+ascion128_lines = plt.plot(tx_powers, [-9, -2, 12], 'o-.g',
+                           color=cipherToColor['ASCON-128'],
+                           label='ASCON-128')
+
+ax.set_yticks(np.arange(y_min, y_lim + 15, y_interval))
+ax.set_xticks([-9, 0, 20])
+ax.set_ylim(y_min, y_lim)
+
 ax.legend(loc='upper right', ncols=3)
-ax.set_ylim(0, y_lim)
+ax.set_ylabel('Energy Consumption Change from AES (%)')
+ax.set_xlabel('TX Power (dBm)')
+ax.set_title('Front Door ESP32-H2')
 
-# red dashes, blue squares and green triangles
-plt.plot([5, 10, 15], 'o--r', color=cipherToColor['AES-CCM'])
-plt.plot([10, 20, 30], 'o:b', color=cipherToColor['ASCON-128a'])
-plt.plot([30, 40, 60], 'o-.g', color=cipherToColor['ASCON-128'])
+plt.axhline(linestyle='dotted', lw=1, color='gainsboro')
 
 plt.show()
