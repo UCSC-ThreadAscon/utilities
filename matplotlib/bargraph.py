fc319e1c1d37dbd0e61bffe390930bc7083d4cb6
Simeon Tran <siatran@ucsc.edu>
2024-04-18 19:58:00 -0700

    Gitignore

--- matplotlib/bargraph.py
+++ matplotlib/bargraph.py
@@ -2,60 +2,7 @@ import matplotlib.pyplot as plt
 import numpy as np
 
 from average import *
-
-files = {
-    "front-door": {
-        "aes": {
-            "0dbm": "aes-0dbm-frontdoor-sample.csv",
-            "20dbm": "aes-20dbm-frontdoor-sample.csv",
-            "-9dbm": "aes-neg9dbm-frontdoor-sample.csv"
-        },
-        "ascon128a": {
-            "0dbm": "",
-            "20dbm": "",
-            "-9dbm": ""
-        },
-        "ascon128": {
-            "0dbm": "",
-            "20dbm": "",
-            "-9dbm": ""
-        }
-    },
-    "washing-machine": {
-        "aes": {
-            "0dbm": "",
-            "20dbm": "",
-            "-9dbm": ""
-        },
-        "ascon128a": {
-            "0dbm": "",
-            "20dbm": "",
-            "-9dbm": ""
-        },
-        "ascon128": {
-            "0dbm": "",
-            "20dbm": "",
-            "-9dbm": ""
-        }
-    },
-    "second-story": {
-        "aes": {
-            "0dbm": "",
-            "20dbm": "",
-            "-9dbm": ""
-        },
-        "ascon128a": {
-            "0dbm": "",
-            "20dbm": "",
-            "-9dbm": ""
-        },
-        "ascon128": {
-            "0dbm": "",
-            "20dbm": "",
-            "-9dbm": ""
-        }
-    }
-}
+from common import *
 
 txpower = ("20 dBm", "0dBm", "-9 dBm")
 mean_energy_usage = {
---
+++ matplotlib/common.py
@@ -0,0 +1,53 @@
+files = {
+    "front-door": {
+        "aes": {
+            "0dbm": "aes-0dbm-frontdoor-sample.csv",
+            "20dbm": "aes-20dbm-frontdoor-sample.csv",
+            "-9dbm": "aes-neg9dbm-frontdoor-sample.csv"
+        },
+        "ascon128a": {
+            "0dbm": "",
+            "20dbm": "",
+            "-9dbm": ""
+        },
+        "ascon128": {
+            "0dbm": "",
+            "20dbm": "",
+            "-9dbm": ""
+        }
+    },
+    "washing-machine": {
+        "aes": {
+            "0dbm": "",
+            "20dbm": "",
+            "-9dbm": ""
+        },
+        "ascon128a": {
+            "0dbm": "",
+            "20dbm": "",
+            "-9dbm": ""
+        },
+        "ascon128": {
+            "0dbm": "",
+            "20dbm": "",
+            "-9dbm": ""
+        }
+    },
+    "second-story": {
+        "aes": {
+            "0dbm": "",
+            "20dbm": "",
+            "-9dbm": ""
+        },
+        "ascon128a": {
+            "0dbm": "",
+            "20dbm": "",
+            "-9dbm": ""
+        },
+        "ascon128": {
+            "0dbm": "",
+            "20dbm": "",
+            "-9dbm": ""
+        }
+    }
+}
