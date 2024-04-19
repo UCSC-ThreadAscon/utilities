3c376a6111af84dc6182921a139d377802fa0640
Simeon Tran <siatran@ucsc.edu>
2024-04-19 11:17:13 -0700

    Added colors to line graph

--- matplotlib/linegraph.py
+++ matplotlib/linegraph.py
@@ -1,9 +1,13 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
+from common import *
+
 # evenly sampled time at 200ms intervals
 t = np.arange(0., 5., 1)
 
 # red dashes, blue squares and green triangles
-plt.plot(t, t, 'o--r', t, t**2, 'o:b', t, t**3, 'o-.g')
+plt.plot(t, t, 'o--r', color=cipherToColor['AES-CCM'])
+plt.plot(t, t**2, 'o:b', color=cipherToColor['ASCON-128a'])
+plt.plot(t, t**3, 'o-.g', color=cipherToColor['ASCON-128'])
 plt.show()
