cca00ce0095eeb02670e95c0c59a9e7ebc5e9e05
Simeon Tran <siatran@ucsc.edu>
2024-04-19 16:50:56 -0700

    To-dos

---
+++ proof/test-decrypt.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+#include "ascon.h"
+
+/**
+ * --- TO-DO ---
+ * 1. A function to get the key
+ * 2. A function to get the nonce
+ * 3. A function to get the associated data
+ * 4. Get the packet bytes.
+ * 4. Then given (1)-(3), decrypt!
+*/
+
+int main() {
+  return 0;
+}
