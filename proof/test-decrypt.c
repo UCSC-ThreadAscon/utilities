c2c44bd7d0522e32ac62a2ba167e71dc6cb77c1a
Simeon Tran <siatran@ucsc.edu>
2024-04-19 17:21:01 -0700

    Test decrypt function

--- proof/test-decrypt.c
+++ proof/test-decrypt.c
@@ -3,10 +3,13 @@
  * 1. A function to get the key
  * 2. A function to get the nonce
  * 3. A function to get the associated data
- * 4. Get the packet bytes.
+ * 4. Get the tag.
+ * 5. Get the packet bytes.
  * 4. Then given (1)-(3), decrypt!
 */
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 #include "ascon.h"
 #include "common.h"
@@ -16,7 +19,7 @@ void* createAssocData(void* dstAddrBytes, void* srcAddrBytes,
 {
   void* assocData = calloc(1, CRYPTO_ABYTES);
 
-  uint16_t offset = (uint16_t) assocData;
+  uint16_t *offset = (uint16_t *) assocData;
 
   memcpy(offset, dstAddrBytes, dstAddrSize);
   offset += dstAddrSize;
@@ -27,6 +30,12 @@ void* createAssocData(void* dstAddrBytes, void* srcAddrBytes,
   return assocData;
 }
 
-int main() {
+int main(void) {
+  uint8_t destination[] = { 0xff, 0xff };
+  uint8_t source[] = { 0x10, 0x68 };
+  void *assocData = createAssocData(destination, source,
+                                    sizeof(destination), sizeof(source));
+
+  free(assocData);
   return 0;
 }
