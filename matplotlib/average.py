db4128ea7bd0692550f8c4cfe1463312a6f9902b
Simeon Tran <siatran@ucsc.edu>
2024-04-22 23:52:35 -0700

    Saving my scripts

--- matplotlib/average.py
+++ matplotlib/average.py
@@ -42,11 +42,11 @@ def getAvgMAh(filename):
   return getMAh(getAvgMa(filename), EXPERIMENT_RUNTIME_HOURS)
 
 def printAvgMa(location, cipher):
-  mA = getAvgMa(files[location][cipher]["9dbm"])
-  print(f"The average mA for {location} {cipher} @ 9dBm is {mA} mA")
-
   mA = getAvgMa(files[location][cipher]["0dbm"])
   print(f"The average mA for {location} {cipher} @ 0dBm is {mA} mA")
+
+  mA = getAvgMa(files[location][cipher]["9dbm"])
+  print(f"The average mA for {location} {cipher} @ 9dBm is {mA} mA")
 
   mA = getAvgMa(files[location][cipher]["20dbm"])
   print(f"The average mA for {location} {cipher} @ 20dBm is {mA} mA")
