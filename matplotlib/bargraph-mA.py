5609b44d6e580616d4e8efa36e742505e7ca0062
Simeon Tran <siatran@ucsc.edu>
2024-04-24 11:31:53 -0700

    Create mA graph for debugging.

--- matplotlib/bargraph-mA.py
+++ matplotlib/bargraph-mA.py
@@ -42,7 +42,7 @@ def bargraph(location, title):
   y_values = []
   for cipher in ["aes", "ascon128a", "ascon128"]:
     for tx in ["0dbm", "9dbm", "20dbm"]:
-      y_values.append(getAvgMAh(prelimData[location][cipher][tx]))
+      y_values.append(prelimData[location][cipher][tx])
 
   y_min = 0
   y_lim = max(y_values) + 0.5
