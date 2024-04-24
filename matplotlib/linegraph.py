6086629987cb7943f5e130d60ecb3e2e67edd273
Simeon Tran <siatran@ucsc.edu>
2024-04-24 11:52:57 -0700

    Function to create line graphs

--- matplotlib/linegraph.py
+++ matplotlib/linegraph.py
@@ -14,42 +14,48 @@ def get_ratios(location, cipher):
     print(f"{cipher} and aes at {tx} has ratio {value_cipher / value_aes}")
     ratio = 1 - (value_cipher / value_aes)
     ratio *= 100
+    ratio *= -1
     ratios.append(ratio)
   return ratios
 
-ascon128a_ratios = get_ratios("front-door", "ascon128a")
-ascon128_ratios = get_ratios("front-door", "ascon128")
 
-all_ratios = ascon128a_ratios + ascon128_ratios
-y_interval = 1.5
-y_lim = max(all_ratios) + y_interval
-y_min = min(all_ratios) - y_interval
+def linegraph(location, title):
+  ascon128a_ratios = get_ratios(location, "ascon128a")
+  ascon128_ratios = get_ratios(location, "ascon128")
 
-print(y_min)
-print(y_lim)
+  all_ratios = ascon128a_ratios + ascon128_ratios
+  y_interval = 1.5
+  y_lim = max(all_ratios) + y_interval
+  y_min = min(all_ratios) - y_interval
 
-fig, ax = plt.subplots()
+  print(y_min)
+  print(y_lim)
 
-# fig.set_figwidth(THESIS_PAPER_WIDTH_IN / 1.2)
-# fig.set_figheight(THESIS_PAPER_HEIGHT_IN / 3)
+  fig, ax = plt.subplots()
 
-ascon128a_lines = plt.plot(TX_POWERS, ascon128a_ratios, 'o:',
-                           color=cipherToColor['ASCON-128a'],
-                           label='ASCON-128a')
-ascion128_lines = plt.plot(TX_POWERS, ascon128_ratios, 'o-.',
-                           color=cipherToColor['ASCON-128'],
-                           label='ASCON-128')
+  # fig.set_figwidth(THESIS_PAPER_WIDTH_IN / 1.2)
+  # fig.set_figheight(THESIS_PAPER_HEIGHT_IN / 3)
 
-ax.set_yticks(np.arange(y_min, y_lim, y_interval))
-ax.set_xticks(TX_POWERS)
-ax.set_ylim(y_min, y_lim)
+  ascon128a_lines = plt.plot(TX_POWERS, ascon128a_ratios, 'o:',
+                            color=cipherToColor['ASCON-128a'],
+                            label='ASCON-128a')
+  ascion128_lines = plt.plot(TX_POWERS, ascon128_ratios, 'o-.',
+                            color=cipherToColor['ASCON-128'],
+                            label='ASCON-128')
 
-ax.legend(loc='best', ncols=3)
-ax.set_ylabel('mAh difference AES (%)')
-ax.set_xlabel('TX Power (dBm)')
-ax.set_title('Front Door Sleepy End Device')
+  ax.set_yticks(np.arange(y_min, y_lim, y_interval))
+  ax.set_xticks(TX_POWERS)
+  ax.set_ylim(y_min, y_lim)
 
-plt.axhline(linestyle='dotted', lw=1, color='gainsboro')
+  ax.legend(loc='best', ncols=3)
+  ax.set_ylabel('mAh difference between AES (%)')
+  ax.set_xlabel('TX Power (dBm)')
+  ax.set_title(title)
 
-# plt.savefig(os.path.join(THESIS_FIGURES_PATH, 'linegraph.pgf'))
-plt.show()
+  plt.axhline(linestyle='dotted', lw=1, color='gainsboro')
+
+  # plt.savefig(os.path.join(THESIS_FIGURES_PATH, 'linegraph.pgf'))
+
+if __name__ == "__main__":
+  linegraph('front-door', 'Front Door Sleepy End Device')
+  plt.show()
