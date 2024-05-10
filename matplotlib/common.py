76d024961d0aed4f1c762527ea00a6f2a04db9f4
Simeon Tran <siatran@ucsc.edu>
2024-05-10 15:21:27 -0700

    Ignore preliminary folder.

--- matplotlib/common.py
+++ matplotlib/common.py
@@ -9,72 +9,14 @@ matplotlib.rcParams.update({
     'pgf.rcfonts': False,
 })
 
+THESIS_FIGURES_PATH = '/Users/simeon/Desktop/Repositories/ThesisWriteup/images/pgfs'
 THESIS_PAPER_WIDTH_IN = 5.75113
 THESIS_PAPER_HEIGHT_IN = 8.12659
 
-THESIS_FIGURES_PATH = '/Users/simeon/Desktop/Repositories/ThesisWriteup/images/pgfs'
-
-"""
-How long each experiment runs for in minutes, hours, and seconds.
-"""
 EXPERIMENT_RUNTIME_MINUTES = 183
 EXPERIMENT_RUNTIME_HOURS = EXPERIMENT_RUNTIME_MINUTES / 60
 
 LOCATIONS = ["front-door", "washing-machine", "second-story"]
-
-files = {
-    "front-door": {
-        "aes": {
-            "0dbm": "csv/front-door/front-door-aes-0dbm.csv",
-            "20dbm": "csv/front-door/front-door-aes-20dbm.csv",
-            "9dbm": "csv/front-door/front-door-aes-9dbm.csv"
-        },
-        "ascon128a": {
-            "0dbm": "csv/front-door/front-door-ascon128a-0dbm.csv",
-            "20dbm": "csv/front-door/front-door-ascon128a-20dbm.csv",
-            "9dbm": "csv/front-door/front-door-ascon128a-9dbm.csv"
-        },
-        "ascon128": {
-            "0dbm": "csv/front-door/front-door-ascon128-0dbm.csv",
-            "20dbm": "csv/front-door/front-door-ascon128-20dbm.csv",
-            "9dbm": "csv/front-door/front-door-ascon128-9dbm.csv"
-        }
-    },
-    "washing-machine": {
-        "aes": {
-            "0dbm": "csv/washingmachine/washingmachine-aes-0dbm.csv",
-            "20dbm": "csv/washingmachine/washingmachine-aes-20dbm.csv",
-            "9dbm": "csv/washingmachine/washingmachine-aes-9dbm.csv"
-        },
-        "ascon128a": {
-            "0dbm": "csv/washingmachine/washingmachine-ascon128a-0dbm.csv",
-            "20dbm": "csv/washingmachine/washingmachine-ascon128a-20dbm.csv",
-            "9dbm": "csv/washingmachine/washingmachine-ascon128a-9dbm.csv"
-        },
-        "ascon128": {
-            "0dbm": "csv/washingmachine/washingmachine-ascon128-0dbm.csv",
-            "20dbm": "csv/washingmachine/washingmachine-ascon128-20dbm.csv",
-            "9dbm": "csv/washingmachine/washingmachine-ascon128-9dbm.csv"
-        }
-    },
-    "second-story": {
-        "aes": {
-            "0dbm": "csv/secondstory/secondstory-aes-0dbm.csv",
-            "20dbm": "csv/secondstory/secondstory-aes-20dbm.csv",
-            "9dbm": "csv/secondstory/secondstory-aes-9dbm.csv"
-        },
-        "ascon128a": {
-            "0dbm": "csv/secondstory/secondstory-ascon128a-0dbm.csv",
-            "20dbm": "csv/secondstory/secondstory-ascon128a-20dbm.csv",
-            "9dbm": "csv/secondstory/secondstory-ascon128-9dbm.csv"
-        },
-        "ascon128": {
-            "0dbm": "csv/secondstory/secondstory-ascon128-0dbm.csv",
-            "20dbm": "csv/secondstory/secondstory-ascon128-20dbm.csv",
-            "9dbm": "csv/secondstory/secondstory-ascon128-9dbm.csv"
-        }
-    }
-}
 
 prelimData = {
     "washing-machine": {
