bcd5c7233dc1bf20a1fb4b3da1d58b4d15d9ca4a
Simeon Tran <siatran@ucsc.edu>
2024-07-03 15:50:08 -0700

    Scripts to grab the times

---
+++ waveforms/waveforms.py
@@ -0,0 +1,47 @@
+import sys
+import csv
+
+MA_WAKEUP_MINIMUM = 1
+UA_WAKEUP_MINIMUM = MA_WAKEUP_MINIMUM * 1000
+
+def getMa(uA):
+  return uA * 0.001
+
+def getMah(mA, hours):
+  return mA * hours
+
+def uAList(filename):
+  microAmpsList = []
+
+  with open(filename) as file:
+    reader = csv.DictReader(file)
+    for row in reader:
+      microAmpCurrent = float(row['Current(uA)'])
+
+      if microAmpCurrent >= UA_WAKEUP_MINIMUM:
+        microAmpsList.append(microAmpCurrent)
+  
+      if len(microAmpsList) >= sys.maxsize:
+        raise OverflowError("The list is too big.")
+
+  return microAmpsList
+
+def getAvgUa(listUa):
+  length = len(listUa)
+  listSum = 0
+
+  for mA in listUa:
+    listSum += mA
+    if (listSum == sys.float_info.max):
+      raise OverflowError("Reach maxed float. Can't add anymore.")
+
+  average = listSum / length
+  return average
+
+def getAvgMa(filename):
+  mAList = uAList(filename)
+  avgUa = getAvgUa(mAList)
+  return getMa(avgUa)
+
+if __name__ == '__main__':
+  pass
