4f1149e81a24d1fe963fd0dd2caa0f377b25016c
Simeon Tran <siatran@ucsc.edu>
2024-07-17 15:24:39 -0700

    Goin to jot down the average delays

--- matplotlib/delay/common.py
+++ matplotlib/delay/common.py
@@ -4,6 +4,9 @@ THESIS_FIGURES_PATH = '/Users/simeon/Desktop/Repositories/ThesisWriteup/images/p
 THESIS_PAPER_WIDTH_IN = 5.75113
 THESIS_PAPER_HEIGHT_IN = 8.12659
 
+CIPHERS = ["ascon128a", "ascon128", "aes", "no encryption"]
+TX_POWERS = ["20 dBm", "9 dBm", "0 dBm"]
+
 RENDER_LATEX = False
 
 if RENDER_LATEX:
@@ -15,3 +18,15 @@ if RENDER_LATEX:
       'text.usetex': True,
       'pgf.rcfonts': False,
   })
+
+averageDelays = \
+{
+  "no encryption": {
+    "20 dBm": "",
+     "9 dBm": "",
+     "0 dBm": ""
+  },
+  "aes": {},
+  "ascon128a": {},
+  "ascon128": {}
+}
